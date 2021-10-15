e724fbee68aeefb6aeb7a0cbc9edeac971159cfd
Carlos Gavidia-Calderon <cgavidiac@gmail.com>
2021-10-15 09:47:03 +0100

    Adding comments

--- src/tsp/bruteforce/BruteForceTsp.java
+++ src/tsp/bruteforce/BruteForceTsp.java
@@ -9,25 +9,28 @@ import java.util.Map;
 public class BruteForceTsp {
 
 
-    public List<String> findOptimalRoute(Map<String, Map<String, Integer>> distanceMap) {
+public List<String> findOptimalRoute(Map<String, Map<String, Integer>> distanceMap) {
 
-        List<String> cities = new ArrayList<>(distanceMap.keySet());
-        List<List<String>> allPermutations =
-                PermutationGenerator.getAllPermutations(cities);
+    List<String> cities = new ArrayList<>(distanceMap.keySet());
+    //Generating all possible permutations.
+    List<List<String>> allPermutations =
+            PermutationGenerator.getAllPermutations(cities);
 
-        List<String> optimalRoute = List.of();
-        int optimalDistance = Integer.MAX_VALUE;
+    List<String> optimalRoute = List.of();
+    int optimalDistance = Integer.MAX_VALUE;
 
-        for (List<String> candidateRoute : allPermutations) {
-            int currentRouteDistance =
-                    TravellingHelper.calculateDistance(candidateRoute, distanceMap);
-            if (currentRouteDistance < optimalDistance) {
-                optimalRoute = candidateRoute;
-                optimalDistance = currentRouteDistance;
-            }
+    for (List<String> candidateRoute : allPermutations) {
+        //Per permutation, we calculate the total distance.
+        int currentRouteDistance =
+                TravellingHelper.calculateDistance(candidateRoute, distanceMap);
+        //If it's shorter than what we found so far, we store it.
+        if (currentRouteDistance < optimalDistance) {
+            optimalRoute = candidateRoute;
+            optimalDistance = currentRouteDistance;
         }
-
-        return optimalRoute;
     }
+
+    return optimalRoute;
+}
 
 }
